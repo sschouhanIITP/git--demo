39fe917191d0cf798ba9320805fb22d8c27d49e2
sschouhanIITP <sschouhan.cse@gmail.com>
2026-01-31 10:39:38 +0530

    added another print

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a sample Java application.");
+        System.out.println("Enjoy coding!");
     }
 }
